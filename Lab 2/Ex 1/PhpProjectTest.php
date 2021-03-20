e18a41b97824919bccd71b1cd5453392c459ab09
incognito1807 <tusupermanvodoi@gmail.com>
2021-03-20 14:14:51 +0700

    add ex1 Lab1

---
+++ Lab 2/Ex 1/PhpProjectTest.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Lab 0</title>
+    </head>
+    <body>
+        <?php
+        echo "We are Fire Ants Team!";
+        echo "Welcome to First PHP Project in Web anđ Online Serveices!";
+        ?>
+    </body>
+</html>
